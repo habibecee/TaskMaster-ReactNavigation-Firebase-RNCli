4227abf6b92124fc9dd5076b29e60273843188f2
habibecee <habibe.ce1996@gmail.com>
2023-05-08 22:14:54 +0300

    project-created-and-firebase-integrated

---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDKKdgIngQZpjysUtKFNtrN-IXXDL1XONA',
+  authDomain: 'todo-app-97b27.firebaseapp.com',
+  projectId: 'todo-app-97b27',
+  storageBucket: 'todo-app-97b27.appspot.com',
+  messagingSenderId: '777519420303',
+  appId: '1:777519420303:web:13d4a8b4f9aa271c4bf1fa',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
